b440f0bbed5b4226b5e5a19f8745cafb5dba12f7
Jeremy CAMHI <jcamhi@student.42.fr>
2018-10-01 20:42:18 +0200

    ex02

--- d00/Assets/ex02/Scripts/Ball.cs
+++ d00/Assets/ex02/Scripts/Ball.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+	private float speed = 0f;
+	private float decelerateSpeed = 2f;
+
+	public void setSpeed(float f)
+	{
+		this.speed = f;
+	}
+
+	public float getSpeed()
+	{
+		return (speed);
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float oldSpeed = speed;
+		if (speed != 0)
+		{
+			this.transform.Translate(0, speed * Time.deltaTime, 0);
+			speed = speed > 0 ? speed - decelerateSpeed * Time.deltaTime : speed + decelerateSpeed * Time.deltaTime;
+			if ((speed < 0 && oldSpeed > 0) || (speed > 0 && oldSpeed < 0))
+				speed = 0;
+		}
 	}
 }
--- d00/Assets/ex02/Scripts/Club.cs
+++ d00/Assets/ex02/Scripts/Club.cs
@@ -5,22 +5,52 @@ using UnityEngine;
 public class Club : MonoBehaviour {
 
 	public Ball ball;
+	public GameObject hole;
 	const float speed = -1.5f;
-
 	private float timePressed;
-	
 	bool descending = false;
+	bool waiting = false;
+	bool ended = false;
 	// Use this for initialization
 	void Start () {
-		
+	}
+
+	float abs(float f)
+	{
+		return (f < 0 ? -1 * f : f);
+	}
+
+	int	find_direction() {
+		if (ball.transform.position.y < hole.transform.position.y)
+			return (-1);
+		else
+			return (1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (ended)
+			return ;
+		if (abs(ball.getSpeed()) < 1)
+		{
+			if (abs(ball.transform.position.y - hole.transform.position.y) < .3)
+			{
+				GameObject.Destroy(ball.gameObject);
+				ended = true;
+			}
+		}
+		if (ball.getSpeed() > 0)
+			return ;
+		if (ball.getSpeed() == 0 && waiting)
+		{
+			waiting = false;
+			this.transform.position = ball.transform.position + new Vector3(0, .7f, 0) * find_direction();
+		}
+
 		if (Input.GetKey(KeyCode.Space))
 		{
 			descending = true;
-			this.transform.Translate(0, speed * Time.deltaTime, 0);
+			this.transform.Translate(0, speed * Time.deltaTime * find_direction() * -1, 0);
 			timePressed += Time.deltaTime;
 		}
 		else if (descending)
@@ -28,11 +58,12 @@ public class Club : MonoBehaviour {
 			hit();
 			descending = false;
 			timePressed = 0;
+			waiting = true;
 		}
 	}
 
 	void hit()
 	{
-		// TODO: Hit depending of how long the space bar was pressed.
+		ball.setSpeed(timePressed * 10 * find_direction() * -1);
 	}
 }
