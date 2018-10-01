faa1350c26e541e33ab867889bfe38864845739f
Jeremy CAMHI <jcamhi@student.42.fr>
2018-10-01 17:38:57 +0200

    ex01

---
+++ d00/Assets/ex01/Scripts/DescendingKey.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DescendingKey : MonoBehaviour {
+	
+	static float	abs(float val)
+	{
+		if (val < 0f)
+			return (val * -1f);
+		else
+			return (val);
+	}
+
+	float speed;
+	public GameObject BottomLine;
+	public GameObject disappearLine;
+	public KeyCode goodKey;
+	void Start () {
+		speed = Random.Range(6, 10) * -1;
+	}
+	
+	void Update () {
+		this.transform.localPosition += new Vector3(0, this.speed * Time.deltaTime, 0);
+		if (this.transform.position.y <= disappearLine.transform.position.y)
+		{
+			GameObject.Destroy(this.gameObject);
+			Debug.Log("Error ! You did not press the key.");
+		}
+		if (Input.GetKeyDown(this.goodKey))
+		{
+			float precision;
+
+			precision = DescendingKey.abs(this.transform.position.y - BottomLine.transform.position.y) * 100;
+			Debug.Log("Precision: " + precision);
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
---
+++ d00/Assets/ex01/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject A;
+	public GameObject S;
+	public GameObject D;
+
+	const float timeBetweenSpawn = 1.8f;
+	float timeUntilNextSpawn;
+	// Use this for initialization
+	void Start () {
+		timeUntilNextSpawn = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeUntilNextSpawn -= Time.deltaTime;
+		if (timeUntilNextSpawn <= 0)
+		{
+			timeUntilNextSpawn = timeBetweenSpawn;
+			spawn();
+		}
+	}
+	void spawn(){
+		int rand;
+		GameObject go;
+
+		rand = Random.Range(0, 3);
+		if (rand == 0)
+			go = this.A;
+		else if (rand == 1)
+			go = this.S;
+		else
+			go = this.D;
+		GameObject.Instantiate(go);
+	}
+}
