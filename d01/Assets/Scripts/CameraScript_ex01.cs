f17ba5e2b8dadc03211633048feb0c8e96c116f5
Jeremy CAMHI <jcamhi@student.42.fr>
2018-10-02 19:42:26 +0200

    Automatic commit

---
+++ d01/Assets/Scripts/CameraScript_ex01.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript_ex01 : MonoBehaviour {
+
+	public playerScript_ex01 red;
+	public playerScript_ex01 blue;
+	public playerScript_ex01 yellow;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.R))
+		{
+			red.reset();
+			blue.reset();
+			yellow.reset();
+			select(red);
+		}
+		handleSelection();
+
+		playerScript_ex01 selected;
+		if (red.selected)
+			selected = red;
+		else if (blue.selected)
+			selected = blue;
+		else
+			selected = yellow;
+		transform.position = new Vector3(selected.transform.position.x, selected.transform.position.y + .4f, transform.position.z);
+		if (red.wellPlaced && blue.wellPlaced && yellow.wellPlaced)
+		{
+			print("You won !");
+		}
+	}
+
+	void handleSelection()
+	{
+		if (Input.GetKey(KeyCode.Alpha1))
+		{
+			select(red);
+			unselect(blue);
+			unselect(yellow);
+		}
+		else if (Input.GetKey(KeyCode.Alpha2))
+		{
+			unselect(red);
+			select(blue);
+			unselect(yellow);
+		}
+		else if (Input.GetKey(KeyCode.Alpha3))
+		{
+			unselect(red);
+			unselect(blue);
+			select(yellow);
+		}
+	}
+
+	void select(playerScript_ex01 p)
+	{
+		if (p.selected)
+			return ;
+		p.selected = true;
+		p.rb2d.constraints &= ~(RigidbodyConstraints2D.FreezePositionX);
+		// p.rb2d.mass /= 100;
+	}
+
+	void unselect(playerScript_ex01 p)
+	{
+		if (!p.selected)
+			return ;
+		p.selected = false;
+		p.rb2d.constraints |= RigidbodyConstraints2D.FreezePositionX;
+	}
+}
---
+++ d01/Assets/Scripts/playerScript_ex02.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript_ex02 : MonoBehaviour {
+	public float speed;
+	public float jump_speed;
+	public bool selected;
+	public string exit_tag;
+	public float	maxSpeed;
+
+	private bool grounded = true;
+	private Vector3 startpos;
+
+
+	[HideInInspector]
+	public Rigidbody2D	rb2d;
+	
+	[HideInInspector]
+	public bool			wellPlaced = false;
+	void Start () {
+		startpos = transform.position;
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if ((other.tag == "wall" || other.tag == "Player") && !other.isTrigger)
+			grounded = true;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == exit_tag)
+			wellPlaced = true;
+	}
+
+	private void OnTriggerExit2D(Collider2D other) {
+		// if ((other.tag == "wall" || other.tag == "player"))
+		// 	grounded = false;
+		if (other.tag == exit_tag)
+			wellPlaced = false;
+	}
+
+	void FixedUpdate () {
+		if (!selected)
+			return ;
+		lateralUpdate();
+		verticalUpdate();
+	}
+
+	void lateralUpdate() {
+		float move = Input.GetAxisRaw("Horizontal");
+		if (move == 0)
+			return ;
+		Vector2 movement = new Vector2(move * speed, 0);
+		if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+			return ;
+		rb2d.AddForce(movement);
+	}
+
+	void verticalUpdate()
+	{
+		if (!grounded)
+			return ;
+		if (Input.GetKey(KeyCode.Space))
+		{
+			rb2d.AddForce(new Vector2(0, jump_speed));
+			grounded = false;
+		}
+	}
+
+	public void reset() {
+		Debug.Log("Reseting");
+		transform.position = startpos;
+		rb2d.velocity = Vector2.zero;
+	}
+}
