b8cdb1857083490954d9fb5130ba55f9beefa1ee
Jeremy CAMHI <jcamhi@student.42.fr>
2018-10-04 16:28:44 +0200

    Automatic commit

---
+++ d03/Assets/Scripts/UI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour {
+
+	public void buttonStart()
+	{
+		SceneManager.LoadScene("ex01");
+	}
+	public void buttonStop()
+	{
+		Application.Quit();
+	}
+
+	public void canonBarClick()
+	{
+		Debug.Log("Clicked on the canon.");
+	}
+	public void gatlingBarClick()
+	{
+		Debug.Log("Clicked on the gatling.");
+	}
+	public void rocketBarClick()
+	{
+		Debug.Log("Clicked on the rocket.");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ d03/Assets/Scripts/UiTurretButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UiTurretButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	// private Image image;
+	private GameObject copy;
+
+	// Use this for initialization
+	void Start () {
+		// image = GetComponent<Image>();
+		// copy = GameObject.Instantiate(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		Debug.Log("OnBeginDrag");
+		copy = GameObject.Instantiate(this.gameObject, transform.position, transform.rotation, transform.parent);
+
+		copy.SetActive(true);
+	}
+
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		Debug.Log("OnDrag");
+		Vector3 pos = Camera.main.ScreenToWorldPoint(eventData.position);
+		pos.z = copy.transform.position.z;
+		copy.transform.position = pos;
+		// transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	 public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log("OnEndDrag");
+		// copy.SetActive(tr);
+	}
+
+}
