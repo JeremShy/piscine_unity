8c71e52f0263619916c76bd40fbd0fa2c32da5e6
Jeremy CAMHI <jcamhi@student.42.fr>
2018-10-04 21:23:58 +0200

    Automatic commit

--- d03/Assets/Scripts/UiTurretButton.cs
+++ d03/Assets/Scripts/UiTurretButton.cs
@@ -7,61 +7,89 @@ using UnityEngine.EventSystems;
 public class UiTurretButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
 	// private Image image;
-	public	GameObject towerPrefab;
+	public gameManager gameManager;
+	public	towerScript towerPrefab;
 	private GameObject copy;
+
+	public GameObject flyIcon;
+	public GameObject noFlyIcon;
+	public Text energyText;
+	public Text damageText;
+	public Text rangeText;
+	public Text waitText;
+
+	private bool dragging = false;
+	private Image img;
 
 	// Use this for initialization
 	void Start () {
-		// image = GetComponent<Image>();
-		// copy = GameObject.Instantiate(gameObject);
+		energyText.text = towerPrefab.energy.ToString();
+		damageText.text = towerPrefab.damage.ToString();
+		rangeText.text = towerPrefab.range.ToString();
+		waitText.text = towerPrefab.fireRate.ToString();
+		if (towerPrefab.type == towerScript.Type.canon) // couldn't find it in the prefabs ?
+		{
+			noFlyIcon.SetActive(true);
+			flyIcon.SetActive(false);
+		}
+		else
+		{
+			noFlyIcon.SetActive(false);
+			flyIcon.SetActive(true);
+		}
+		img = GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (gameManager.playerEnergy < towerPrefab.energy && (img.color != Color.red))
+			img.color = Color.red;
+		else if (gameManager.playerEnergy >= towerPrefab.energy && (img.color == Color.red))
+			img.color = Color.white;
 	}
 
 	public void OnBeginDrag(PointerEventData eventData)
 	{
-		Debug.Log("OnBeginDrag");
-		copy = GameObject.Instantiate(this.gameObject, transform.position, transform.rotation, transform.parent);
-
-		copy.SetActive(true);
+		if (gameManager.playerEnergy < towerPrefab.energy)
+			dragging = false;
+		else
+		{
+			dragging = true;
+			copy = GameObject.Instantiate(this.gameObject, transform.position, transform.rotation, transform.parent);
+			copy.SetActive(true);
+		}
 	}
 
 
 	public void OnDrag(PointerEventData eventData)
 	{
-		Vector3 pos = Camera.main.ScreenToWorldPoint(eventData.position);
-		pos.z = copy.transform.position.z;
-		copy.transform.position = pos;
+		if (dragging == true)
+		{
+			Vector3 pos = Camera.main.ScreenToWorldPoint(eventData.position);
+			pos.z = copy.transform.position.z;
+			copy.transform.position = pos;
+		}
 	}
 
 	 public void OnEndDrag(PointerEventData eventData)
 	{
-		Debug.Log("OnEndDrag");
-		// GameObject.Destroy(copy);
-		Vector3 pos = Camera.main.ScreenToWorldPoint(eventData.position);
-		pos.z = 10;
+		if (dragging == true)
+		{
+			dragging = false;
+			Vector3 pos = Camera.main.ScreenToWorldPoint(eventData.position);
+			pos.z = 10;
 
-		RaycastHit2D hit = Physics2D.Raycast(pos , new Vector3(0, 0, -1));
-		if (hit.collider == null)
-		{
-			Debug.Log("No collider.");
-		}
-		else
-		{
-			if (hit.collider.tag != "empty")
+			RaycastHit2D hit = Physics2D.Raycast(pos , new Vector3(0, 0, -1));
+			if (hit.collider != null)
 			{
-				Debug.Log("Collision not empty");
+				if (hit.collider.tag == "empty")
+				{
+					gameManager.playerEnergy -= towerPrefab.energy;
+					GameObject.Instantiate(towerPrefab, hit.collider.transform.position, hit.collider.transform.rotation, hit.collider.transform.parent);
+				}
 			}
-			else
-			{
-				Debug.Log("Collided an empty tile !");
-				GameObject.Instantiate(towerPrefab, hit.collider.transform.position, hit.collider.transform.rotation, hit.collider.transform.parent);
-			}
+			GameObject.Destroy(copy);
 		}
-		GameObject.Destroy(copy);
 	}
 
 }
---
+++ d03/Assets/Scripts/playerPanelScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class playerPanelScript : MonoBehaviour {
+
+	// Use this for initialization
+	public Text hpText;
+	public Text energyText;
+	public gameManager gameManager;
+	void Start () {
+		refresh();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		refresh();
+	}
+
+	void refresh()
+	{
+		hpText.text = gameManager.playerHp.ToString();
+		energyText.text = gameManager.playerEnergy.ToString();
+	}
+}
