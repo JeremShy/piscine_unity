ebf694ced21fa39ba5a052140016f2f947818141
Jeremy CAMHI <jcamhi@student.42.fr>
2018-10-05 17:56:17 +0200

    Automatic commit

---
+++ d00/ex01/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+	public GameObject CubePrefab1;
+	public GameObject CubePrefab2;
+	public GameObject CubePrefab3;
+	public GameObject[] tabCube;
+
+	public float[] tabX;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Cube spawner initiated.\n");
+		tabCube = new GameObject[3];
+		tabCube [0] = CubePrefab1;
+		tabCube [1] = CubePrefab2;
+		tabCube [2] = CubePrefab3;
+
+		tabX = new float[3];
+		tabX [0] = -3.3f;
+		tabX [1] = 0f;
+		tabX [2] = 3.3f;
+
+		Invoke ("spawnCube", 0.5f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void spawnCube() {
+		float randomTime = Random.Range (0.85f, 1f);
+
+		int type = Random.Range (0, 3);
+		Instantiate (tabCube[type], new Vector3 (tabX [type], 4, -1), transform.rotation);
+
+		Invoke ("spawnCube", randomTime);
+	}
+}
---
+++ d00/ex02/Assets/Scripts/Club.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Club : MonoBehaviour {
+    private bool _state = false;
+    private float _niveau = 0f;
+    private Vector3 _start;
+
+	// Use this for initialization
+	void Start () {
+        hide();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            power();
+        }
+        else if (_state)
+        {
+            hide();
+            _niveau = 0;
+        }
+    }
+
+    void power()
+    {
+        if (_state == false)
+        {
+            show();
+            _niveau = 1;
+            _start = transform.position;
+            return;
+        }
+        else if (_niveau < 100)
+        {
+            _start.y += -2 * Time.deltaTime;
+            _niveau += 400 * Time.deltaTime;
+            if (_niveau > 100)
+                _niveau = 100;
+            transform.position = _start;
+        }
+        Debug.Log("_niveau : " + _niveau);
+    }
+
+    void hide()
+    {
+        Vector3 vec = this.transform.position;
+        vec.z = 15;
+        this.transform.position = vec;
+        _state = false;
+    }
+
+    void show()
+    {
+        Vector3 vec = this.transform.position;
+        vec.z = 0;
+        this.transform.position = vec;
+        _state = true;
+    }
+}
